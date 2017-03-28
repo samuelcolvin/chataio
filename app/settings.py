35843bbc93715f7e882d393d7eab1ed975496f44
Samuel Colvin <s@muelcolvin.com>
2017-03-28 18:25:49 +0100

    accept DATABASE_URL

--- app/settings.py
+++ app/settings.py
@@ -1,10 +1,15 @@
-from aiohttp_prodtools.settings import BaseSettings, Required
+import os
+from aiohttp_prodtools.settings import BaseSettings
 
 
 class Settings(BaseSettings):
     DB_NAME = 'chataio'
     DB_USER = 'postgres'
-    DB_PASSWORD = Required(str)
+    DB_PASSWORD = None
     DB_HOST = 'localhost'
     DB_PORT = '5432'
-    COOKIE_SECRET = Required(str)
+    COOKIE_SECRET = 'paVv-Da51WqaDEqseFuepIKpfl8PifBT1xQmVgbpEQ8='
+
+    @property
+    def db_dsn(self):
+        return os.getenv('DATABASE_URL', None) or super().db_dsn
